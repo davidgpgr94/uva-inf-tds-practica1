545d0dcd0f0de7cc9cfdc189df5bd538865f0b69
davidgo <david.gonzalez.perez@alumnos.uva.es>
2017-11-03 22:04:54 +0100

    Actualizada la implementación de Libreta.java

--- src/uva/tds/pr1/equipo09/Libreta.java
+++ src/uva/tds/pr1/equipo09/Libreta.java
@@ -4,11 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Libreta {
-	
-	private Map<String, Contact> contactos= new HashMap<>();
-	
+
+	private Map<String, Contact> contactos;
+
 	public Libreta() {
 		// TODO Auto-generated constructor stub
+		contactos = new HashMap<String, Contact>();
 	}
 
 	public Map<String, Contact> getContactos() {
@@ -16,13 +17,20 @@ public class Libreta {
 	}
 
 	public void setContactos(Map<String, Contact> contactos) {
-		this.contactos = contactos;
+		for (Contact contacto : contactos.values()) {
+			añadirContacto(contacto);
+		}
+		//this.contactos = contactos; //esto es lo que estaba antes
 	}
-	
-	public void añadirContacto(Contact contacto){
-		contactos.put(contacto.getId(), contacto);
+
+	public void añadirContacto(Contact contacto) {
+		if (contactos.get(contacto.getId()) == null) {
+			contactos.put(contacto.getId(), contacto);
+		} else {
+			throw new IllegalArgumentException("El id (" + contacto.getId() + ") del contacto ya existe en la libreta");
+		}
 	}
-	
+
 	public Contact getContacto(String id) {
 		return contactos.get(id);
 	}
