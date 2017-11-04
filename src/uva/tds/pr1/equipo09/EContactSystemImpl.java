bd18df187e991b5375a29bd127d9d6424837cf57
davidgo <david.gonzalez.perez@alumnos.uva.es>
2017-11-04 17:52:25 +0100

    Eliminado el import de DefaultHandler

--- src/uva/tds/pr1/equipo09/EContactSystemImpl.java
+++ src/uva/tds/pr1/equipo09/EContactSystemImpl.java
@@ -14,7 +14,6 @@ import javax.xml.parsers.SAXParserFactory;
 
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
-import org.xml.sax.helpers.DefaultHandler;
 
 public class EContactSystemImpl implements EContactSystemInterface {
 
